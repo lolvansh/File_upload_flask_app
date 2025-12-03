0d22f850ff275773a7524cdb52f4dd68bc7a72a1
lolvansh <lolvansh577@gmail.com>
2025-12-03 12:15:45 +0530

    upload file api complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import FileUploader from "./components/FileUploader";
+
+function app(){
+  return (
+    <div className="App">
+      <h1>My File Upload App</h1>
+      <FileUploader />
+
+    </div>
+  )
+}
+
+export default app
---
+++ frontend/src/components/FileUploader.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+
+
+
+function FileUploader(){
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [status, setStatus] = useState(null)
+
+    const handleFileSelect = (event) =>{
+        const file = event.target.files[0];
+
+        setSelectedFile(file);
+        setStatus("File Selected: " +file.name);
+    }
+
+    const handleFileUpload = async () => {
+        if(!selectedFile){
+            setStatus("please select a file first!");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", selectedFile)
+        setStatus("Uploading...");
+
+        try{
+            const response = await fetch("/api/upload",
+                {
+                    method: "POST",
+                    body: formData
+                }
+            );
+
+            if (response.ok){
+                setStatus("Success! File Uploaded")
+            } else{
+                const errorData = await response.json();
+                
+                setStatus("Error:"+errorData.message);
+            }
+        }
+        catch (error){
+            console.error("ERROR:", error);
+            setStatus("Netword Error.")
+            
+        }
+        
+
+
+    }
+    return (
+        <div className="uploader-container">
+            <input type="file" onChange={handleFileSelect}/>
+            <button onClick={handleFileUpload}>Upload</button>
+            <p>Status: {status} </p>
+        </div>
+    ) 
+}
+
+export default FileUploader
