aaecd7fad9568b061fdff2f2b349cbfd69230c5f
lolvansh <lolvansh577@gmail.com>
2025-12-03 11:27:39 +0530

    frontend api config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000', // Make sure this matches your Flask port!
+        changeOrigin: true,
+        secure: false,      
+        
+        // 1. We removed '_options' from here because we don't use it
+        configure: (proxy) => { 
+          
+          // 2. We removed '_req' and '_res' from here
+          proxy.on('error', (err) => { 
+            console.log('proxy error', err);
+          });
+
+          // 3. We removed '_res' from here
+          proxy.on('proxyReq', (proxyReq, req) => { 
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+
+          // 4. We removed '_res' from here too
+          proxy.on('proxyRes', (proxyRes, req) => { 
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
+  }
+});
