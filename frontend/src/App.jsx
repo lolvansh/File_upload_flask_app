723461b445b77609ec3b68c19b5fbd5ee8ef133b
lolvansh <lolvansh577@gmail.com>
2025-12-04 11:40:16 +0530

    changes in the toast css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,47 @@ function App(){
 
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4">
-      <Toaster position="top-right" reverseOrder={false} />
+      <Toaster position="top-right" reverseOrder={false} 
+                gutter={8}
+                toastOptions={{
+                  duration: 4000,
+                  success: {
+                    style: {
+                    background: '#ecfdf5', // green-50
+                    color: '#15803d',      // green-700
+                    border: '1px solid #bbf7d0', // green-200
+                    padding: '16px',
+                    fontWeight: '500', 
+                    },
+                    iconTheme: {
+                      primary: '#1d3f50ff',
+                      secondary: '#ecfd15',
+                    },
+                  },
+                  error: {
+                      style: {
+                          background: '#fef2f2', // red-50
+                          color: '#b44c4cff',      // red-700
+                          border: '1px solid #f1dedeff', // red-200
+                          padding: '16px',
+                          fontWeight: '500',
+                      },
+                      iconTheme: {
+                          primary: '#b91c1c',
+                          secondary: '#fef2f2',
+                      },
+                  },
+                  loading: {
+                      style: {
+                          background: 'white',
+                          color: '#374151',      // gray-700
+                          border: '1px solid #e5e7eb', // gray-200
+                          padding: '16px',
+                      },
+                  }
+                }}
+                
+                />
 
       <div className="max-w-4xl mx-auto space-y-8">
 
