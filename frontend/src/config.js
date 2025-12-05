5521f962c78cdd54537e3974a7e7a2453a9f94f5
lolvansh <lolvansh577@gmail.com>
2025-12-05 13:16:20 +0530

    config changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 // export const API_URL = import.meta.env.VITE_API_URL || "";
 
-export const API_URL = "http://127.0.0.1:5000"
+// src/config.js
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
